5301ab538a948dab0ef73edaa48afd534a146c0f
Erick El nino <erick.tshimbombo@yahoo.com>
2023-09-11 19:51:21 +0200

    update my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,59 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
+import "./App.css";
+import { useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0);
+  const [Converted, setConveretd] = useState(0.0);
+  const [firstCurrenty, setFirstCurrency] = useState("");
+  const [secondCurrenty, setSecondCurrency] = useState("USD");
+  const [Amount, setAmount] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    if (firstCurrenty && secondCurrenty && Amount) {
+      const fetchData = async () => {
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?amount=${Number(
+            Amount
+          )}&from=${firstCurrenty}&to=${secondCurrenty}`,
+          { signal: controller.signal }
+        );
+        const data = await response.json();
+        setConveretd(data.rates[secondCurrenty]);
+      };
+
+      fetchData();
+
+      return function () {
+        controller.abort();
+      };
+    }
+  }, [firstCurrenty, secondCurrenty, Amount]);
 
   return (
     <>
       <div>
-        <input type="text" />
-        <select>
+        <input
+          type="text"
+          value={Amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <select onChange={(e) => setFirstCurrency(e.target.value)}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
           <option value="INR">INR</option>
         </select>
-        <select>
+        <select onChange={(e) => setSecondCurrency(e.target.value)}>
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
           <option value="CAD">CAD</option>
           <option value="INR">INR</option>
         </select>
-        <p>OUTPUT</p>
+        <p>
+          {Converted} {secondCurrenty}
+        </p>
       </div>
     </>
   );
